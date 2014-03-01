71b07d3e53751b4e33875edfb17067ef9329d3d1
Ramiro Jr. Franco <rjfranco@gmail.com>
2014-03-01 13:23:45 -0800

    Removing unused rabl components

--- app.rb
+++ app.rb
@@ -12,9 +12,4 @@ class App < Sinatra::Base
   get '/' do
     haml :index
   end
-
-  get '/food' do
-    @food = {id: '1', category: 'cheetos', name: 'AM PM', address: '1110 SE Powell Blvd, Portland, OR 97202', rating: '5', source: 'foursquare'}
-    rabl :food, format: 'json'
-  end
 end
