f2d642105a130b180288959fa5710680e62b1aba
Matthew Boeh <matthew.boeh@gmail.com>
2014-03-01 13:48:15 -0800

    Create an in-memory session store and adjust the response format.

--- api.rb
+++ api.rb
@@ -1,13 +1,65 @@
 Bundler.require
+
+require 'securerandom'
 require 'sinatra/json'
+
+class SessionStore
+
+  class Session < Struct.new(:token, :store)
+    def data
+      store[token] ||= {}
+    end
+
+    def stored_results?
+      data[:results]
+    end
+
+    def store_results(results)
+      data[:results] = results
+    end
+
+    def fetch_result(index)
+      index = index.to_i % data[:results].length
+      return data[:results][index], index+1
+    end
+
+    def to_json(*a)
+      { token: token }.to_json(*a)
+    end
+  end
+
+  def initialize
+    @data = {}
+  end
+
+  def start(token = nil)
+    token = generate_token if token.nil? or token.empty?
+    Session.new(token, @data)
+  end
+
+  private
+
+  def generate_token
+    SecureRandom.hex
+  end
+
+end
 
 class API < Sinatra::Base
   helpers Sinatra::JSON
 
   get '/gimme' do
-    results = client.retrieve
+    authenticate do |session|
+      unless params[:next] && session.stored_results?
+        session.store_results client.retrieve
+      end
+      
+      result, next_id = session.fetch_result(params[:next])
 
-    json "results" => results.map(&:to_h)
+      json "result"  => result.to_h,
+           "next"    => url("/gimme?next=#{next_id}&token=#{session.token}"),
+           "session" => session
+    end
   end
 
   private
@@ -18,6 +70,14 @@ class API < Sinatra::Base
         client.add_source Burgatron::Sources::Canned.new(path: "canned.yml")
       end
     end
+  end
+
+  def sessions
+    @@sessions ||= SessionStore.new
+  end
+
+  def authenticate(&blk)
+    yield sessions.start(params[:token])
   end
 
 end
