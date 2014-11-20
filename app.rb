189f695565c0617e21b347cce27b1bb42822246c
Ramiro Jr. Franco <rjfranco@gmail.com>
2014-11-19 22:21:33 -0800

    Removing js compressor specification

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_js_compressor, :none
   set :assets_precompile, %w(*.js *.css *.png *.jpg *.svg *.eot *.ttf *.woff)
   register Sinatra::AssetPipeline
 
